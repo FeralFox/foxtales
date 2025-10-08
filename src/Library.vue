83a7eac7eaadfbe7ddbda009c706335fdac88c19
pyfox <me@pyfox.net>
2025-10-08 22:28:49 +0200

    Faster book display - load covers after

--- src/Library.vue
+++ src/Library.vue
@@ -244,6 +244,13 @@ async function loadBooks(start_from: number, initialFetch?: boolean) {
   if (fetchedBooks.length === 0) {
     return
   }
+  // Immediately display all books as soon as they are available.
+  if (start_from === 0) {
+    books.value = fetchedBooks
+  } else {
+    books.value = [...books.value, ...fetchedBooks]
+  }
+
   // prefetch covers as data urls (requires auth header)
   const map: Record<string, string> = {}
   await Promise.all(fetchedBooks.map(async (b: any) => {
@@ -255,13 +262,15 @@ async function loadBooks(start_from: number, initialFetch?: boolean) {
     } catch {
     }
   }))
+  // Display the covers
   if (start_from === 0) {
-    books.value = fetchedBooks
     covers.value = map
   } else {
-    books.value = [...books.value, ...fetchedBooks]
     covers.value = {...covers.value, ...map}
   }
+
+  // Wait for all books and covers to be displayed - then render everything - then check if there are scrollbars.
+  // Then check if we need to fetch more books to fill the page.
   await nextTick()
   let hasScrollBars = bookContainer.value!.scrollHeight > bookContainer.value!.clientHeight;
   let mightHaveAdditionalBooks = fetchedBooks.length === BOOKS_TO_PREFETCH;
