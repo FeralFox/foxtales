f59815ed961ca0606dcb9dabca799d9013881f62
pyfox <me@pyfox.net>
2025-11-15 22:56:42 +0100

    Move modules back to keep foliatejs structure intact; import from original directory to force the browser to download all necessary dependencies to work offline on page load.

--- vite.config.ts
+++ vite.config.ts
@@ -1,52 +1,47 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
 import vue from '@vitejs/plugin-vue'
 import svgLoader from 'vite-svg-loader'
-import { VitePWA } from 'vite-plugin-pwa'
+import {VitePWA} from 'vite-plugin-pwa'
+
 
 // https://cn.vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    cssInjectedByJsPlugin(),
-    svgLoader({
-      defaultImport: 'component',
-    }),
-    VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: [
-        'favicon.ico',
-        'icons/logo.svg',
-        'icons/logo192.png',
-        'icons/logo512.png',
-      ],
-      manifest: {
-        name: 'Foxtales Ebook Library',
-        short_name: 'Foxtales',
-        description: 'An ebook library and reader.',
-        theme_color: '#fff',
-        background_color: '#fff',
-        display: 'standalone',
-        scope: '/',
-        start_url: '/',
-        icons: [
-          {
-            src: '/icons/logo192.png',
-            sizes: '192x192',
-            type: 'image/png',
-            purpose: 'any maskable',
-          },
-          {
-            src: '/icons/logo512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any maskable',
-          },
-        ],
-      },
-    }),
-  ],
-  build: {
-    target: 'esnext', //browsers can handle the latest ES features
-  },
+    plugins: [vue(),
+        cssInjectedByJsPlugin(),
+        svgLoader({
+            defaultImport: "component"
+        }),
+        VitePWA({
+            registerType: 'autoUpdate',
+            includeAssets: ['favicon.ico', 'icons/logo.svg', 'icons/logo192.png', 'icons/logo512.png'],
+            manifest: {
+                name: 'Foxtales Ebook Library',
+                short_name: 'Foxtales',
+                description: 'An ebook library and reader.',
+                theme_color: '#fff',
+                background_color: '#fff',
+                display: 'standalone',
+                scope: '/',
+                start_url: '/',
+                icons: [
+                    {
+                        src: '/icons/logo192.png',
+                        sizes: '192x192',
+                        type: 'image/png',
+                        purpose: 'any maskable'
+                    },
+                    {
+                        src: '/icons/logo512.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                        purpose: 'any maskable'
+                    }
+                ]
+            }
+        })
+    ],
+    build: {
+        target: 'esnext' //browsers can handle the latest ES features
+    },
 })
